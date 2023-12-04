e7fe2bf84487a26b43ee1470aeecab879e47a6b2
maximumry <maximum.river1211@gmail.com>
2023-12-04 16:46:51 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/grandprixpulse/GrandPrixPulseApplication.java
@@ -0,0 +1,13 @@
+package in.techcamp.grandprixpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandPrixPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandPrixPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/grandprixpulse/GrandPrixPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.grandprixpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandPrixPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
